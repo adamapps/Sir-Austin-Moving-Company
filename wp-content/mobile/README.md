4b0d8772bedf7f9cd5437072be61f51bafb337df
adamapps <adamdartez@gmail.com>
2016-11-13 21:24:44 -0600

    Added random snapshot

---
+++ wp-content/mobile/README.md
@@ -0,0 +1,2 @@
+# MOOVER MANUAL README:
+ fdsafds
---
+++ wp-content/mobile/www/js/config.js
@@ -0,0 +1,6 @@
+angular.module('your_app_name.config', [])
+.constant('WORDPRESS_API_URL', 'http://192.168.0.2/api/')
+.constant('WORDPRESS_PUSH_URL', 'https://wordpress.startapplabs.com/blog/push/')
+.constant('GCM_SENDER_ID', '574597432927')
+
+;
